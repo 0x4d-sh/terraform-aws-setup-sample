76634add564da44f21022db67a33dfbcad33fab4
0x4d <74593196+0x4d-sh@users.noreply.github.com>
2021-07-23 00:59:45 +0800

    Create variables.tf

---
+++ v2/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+    type = string
+    description = "The AWS region things are created in"
+}
+
+variable "aws_s3_bucket" {
+    type = string
+    description = "The AWS region things are created in"
+}
+
+variable "aws_iam_profile" {
+    type = string
+    description = "The AWS region things are created in"
+}
