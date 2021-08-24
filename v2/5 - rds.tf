8ab2f3e10e0fb97034a277ab4737d4d5ced9bcda
0x4d <74593196+0x4d-sh@users.noreply.github.com>
2021-08-24 18:42:30 +0800

    Update 5 - rds.tf

--- v2/5 - rds.tf
+++ v2/5 - rds.tf
@@ -10,7 +10,7 @@ resource "aws_rds_cluster" "default" {
   backup_retention_period = 30
   preferred_backup_window = "07:00-09:00"
 
-  db_subnet_group_name    = "${aws_subnet.private.*.name}"
+  db_subnet_group_name    = "${aws_subnet.private.0.name}"
   vpc_security_group_ids     = ["${aws_security_group.rds_sg.id}"]
   enabled_cloudwatch_logs_exports = ["audit","error","general","slowquery"]
 
