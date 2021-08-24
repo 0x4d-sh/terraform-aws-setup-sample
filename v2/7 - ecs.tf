157ae4ae60b33ae98ae8301d79109d0ff70e1cea
0x4d <74593196+0x4d-sh@users.noreply.github.com>
2021-08-24 21:51:52 +0800

    Add ecs server

--- v2/7 - ecs.tf
+++ v2/7 - ecs.tf
@@ -4,51 +4,51 @@ resource "aws_ecs_cluster" "aws_ecs_cluster" {
   name = "${var.app_name}-${var.app_environment}-cluster"
 }
 
-# data "template_file" "in_app" {
-#   template = file("./templates/ecs/app.json.tpl")
+data "template_file" "in_app" {
+  template = file("./templates/app.json.tpl")
 
-#   vars = {
-#     app_name        = var.app_name
-#     app_environment = var.app_environment
-#     app_image       = var.app_image
-#     app_port        = var.app_port
-#     fargate_cpu     = var.fargate_cpu
-#     fargate_memory  = var.fargate_memory
-#     aws_region      = var.aws_region
-#   }
-# }
+  vars = {
+    app_name        = var.app_name
+    app_environment = var.app_environment
+    app_image       = var.app_image
+    app_port        = var.app_port
+    fargate_cpu     = var.fargate_cpu
+    fargate_memory  = var.fargate_memory
+    aws_region      = var.aws_region
+  }
+}
 
-# resource "aws_ecs_task_definition" "app" {
-#   family                   = "${var.app_name}-${var.app_environment}-task"
-#   execution_role_arn       = var.ecs_task_execution_arn
-#   network_mode             = "awsvpc"
-#   requires_compatibilities = ["FARGATE"]
-#   cpu                      = var.fargate_cpu
-#   memory                   = var.fargate_memory
-#   container_definitions    = data.template_file.in_app.rendered
-# }
+resource "aws_ecs_task_definition" "app" {
+  family                   = "${var.app_name}-${var.app_environment}-task"
+  execution_role_arn       = var.ecs_task_execution_arn
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = var.fargate_cpu
+  memory                   = var.fargate_memory
+  container_definitions    = data.template_file.in_app.rendered
+}
 
-# resource "aws_ecs_service" "aws_ecs_service" {
-#   name            = "${var.app_name}-${var.app_environment}-service"
-#   cluster         = aws_ecs_cluster.aws_ecs_cluster.id
-#   task_definition = aws_ecs_task_definition.app.arn
-#   desired_count   = var.app_count
-#   launch_type     = "FARGATE"
+resource "aws_ecs_service" "aws_ecs_service" {
+  name            = "${var.app_name}-${var.app_environment}-service"
+  cluster         = aws_ecs_cluster.aws_ecs_cluster.id
+  task_definition = aws_ecs_task_definition.app.arn
+  desired_count   = var.app_count
+  launch_type     = "FARGATE"
 
-#   network_configuration {
-#     security_groups  = [aws_security_group.ecs_sg.id]
-#     subnets          = aws_subnet.private.*.id
-#     assign_public_ip = true
-#   }
+  network_configuration {
+    security_groups  = [aws_security_group.ecs_sg.id]
+    subnets          = aws_subnet.private.*.id
+    assign_public_ip = true
+  }
 
-#   load_balancer {
-#     target_group_arn = aws_alb_target_group.target_group.id
-#     container_name   = "${var.app_name}-${var.app_environment}"
-#     container_port   = var.app_port
-#   }
+  load_balancer {
+    target_group_arn = aws_alb_target_group.target_group.id
+    container_name   = "${var.app_name}-${var.app_environment}"
+    container_port   = var.app_port
+  }
 
-#   depends_on = [
-#     aws_alb_listener.front_end, 
-#     # aws_iam_role_policy_attachment.ecs_task_execution_role
-#   ]
-# }
+  depends_on = [
+    aws_alb_listener.front_end, 
+    # aws_iam_role_policy_attachment.ecs_task_execution_role
+  ]
+}
