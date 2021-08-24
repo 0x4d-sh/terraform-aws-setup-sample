f2833cd01e310d0ccca85b32e97125be61541f9a
0x4d <74593196+0x4d-sh@users.noreply.github.com>
2021-08-24 21:56:35 +0800

    Update variables.tf

--- v2/variables.tf
+++ v2/variables.tf
@@ -59,3 +59,23 @@ variable "health_check_path" {
     type = string
     description = "Health Check URL path"
 }
+
+variable "app_image" {
+    type = string
+    description = "Docker image to run in the ECS cluster"
+}
+
+variable "app_port" {
+    type = number
+    description = "Port exposed by the docker image to redirect traffic to"
+}
+
+variable "fargate_cpu" {
+    type = string
+    description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)"
+}
+
+variable "fargate_memory" {
+    type = string
+    description = "Fargate instance memory to provision (in MiB)"
+}
