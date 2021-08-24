a9bbe156be633b58ef1e1808c590d372aad07c97
0x4d <74593196+0x4d-sh@users.noreply.github.com>
2021-08-24 18:32:59 +0800

    Update 5 - rds.tf

--- v2/5 - rds.tf
+++ v2/5 - rds.tf
@@ -3,14 +3,14 @@ resource "aws_rds_cluster" "default" {
   cluster_identifier      = "${var.app_name}-${var.app_environment}-rds"
   engine                  = "aurora-mysql"
   engine_version          = "5.7.mysql_aurora.2.03.2"
-  availability_zones      = ["${aws_security_group.rds_sg.id}"]
+  availability_zones      = [data.aws_availability_zones.available.name]
   database_name           = var.db_name
   master_username         = var.db_user
   master_password         = var.db_password
   backup_retention_period = 30
   preferred_backup_window = "07:00-09:00"
 
-  vpc_security_group_ids     = [aws_security_group.alb.id]
+  vpc_security_group_ids     = ["${aws_security_group.rds_sg.id}"]
   enabled_cloudwatch_logs_exports = ["audit","error","general","slowquery"]
 
   tags = {
