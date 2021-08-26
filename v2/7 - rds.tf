4081d47976f6fbe5df9e8dc288bacceedf081632
0x4d <74593196+0x4d-sh@users.noreply.github.com>
2021-08-26 23:03:10 +0800

    Update 7 - rds.tf

--- v2/7 - rds.tf
+++ v2/7 - rds.tf
@@ -10,14 +10,32 @@ resource "aws_db_subnet_group" "rds" {
 
 resource "aws_rds_cluster" "default" {
   cluster_identifier      = "${var.app_name}-${var.app_environment}-rds"
+
+  db_subnet_group_name    = "${aws_db_subnet_group.rds.name}"
+  vpc_security_group_ids  = [aws_security_group.rds_sg.id, aws_security_group.ecs_sg.id]
+
+  skip_final_snapshot     = true
+
+  tags = {
+    Name        = "${var.app_name}-rds"
+    Environment = var.app_environment
+  }
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage       = 20
+  max_allocated_storage   = 200
+
   engine                  = "mysql"
   engine_version          = "5.7.34"
+  instance_class          = "db.m3"
+
   database_name           = var.db_name
   master_username         = var.db_user
   master_password         = aws_secretsmanager_secret.database_password_secret.arn
 
-  allocated_storage       = 100
-  max_allocated_storage   = 200
+  allow_major_version_upgrade = true
+  allow_minor_version_upgrade = true
 
   maintenance_window      = "Mon:00:00-Mon:03:00"
   backup_window           = "03:00-06:00"
@@ -27,18 +45,13 @@ resource "aws_rds_cluster" "default" {
   monitoring_role_name    = "AWSServiceRoleForRDS"
   create_monitoring_role  = false
 
-  auto_minor_version_upgrade = "30"
-  performance_insights_enabled = true
+  auto_minor_version_upgrade            = "30"
+  performance_insights_enabled          = true
   performance_insights_retention_period = 30
-  enabled_cloudwatch_logs_exports = ["audit", "error", "general", "slowquery"]
+  enabled_cloudwatch_logs_exports       = ["audit", "error", "general", "slowquery"]
 
   backup_retention_period = 30
   preferred_backup_window = "07:00-09:00"
-
-  db_subnet_group_name    = "${aws_db_subnet_group.rds.name}"
-  vpc_security_group_ids  = [aws_security_group.rds_sg.id, aws_security_group.ecs_sg.id]
-
-  skip_final_snapshot     = true
 
   tags = {
     Name        = "${var.app_name}-rds"
